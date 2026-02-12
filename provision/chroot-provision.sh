3a91fcb91c47d76be1165f50a59ba62106eca31d
Jim Narey <jimnarey@me.com>
2026-02-12 19:24:43 +0000

    Fix package name

--- provision/chroot-provision.sh
+++ provision/chroot-provision.sh
@@ -12,7 +12,7 @@ apt-get install -y --no-install-recommends ca-certificates curl gnupg lsb-releas
   software-properties-common
 
 echo "Installing basic tools..."
-apt-get install -y --no-install-recommends git build-essential nano emacs
+apt-get install -y --no-install-recommends git build-essential nano emacsen-common
 
 # Prevent services from being started during package installation
 cat >/usr/sbin/policy-rc.d <<'EOF'
